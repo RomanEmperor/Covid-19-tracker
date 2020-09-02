84f1809359289134987675dd61df536afe90c76c
Roman Parajuli <notorious.garage@gmail.com>
2020-09-02 16:52:53 +0545

    Initial Commit

---
+++ Covid/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Covid
+//
+//  Created by Roman Parajuli on 9/2/20.
+//  Copyright © 2020 Roman Parajuli. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
